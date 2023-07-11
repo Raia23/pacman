6fd386037421bda7c5e42551d9b2b7d2f89c5ed3
jcquadros <julliepaper@gmail.com>
2023-07-10 23:38:01 -0300

    pronto

--- main.c
+++ main.c
@@ -8,211 +8,282 @@
 #define MAX_FANTASMAS 10
 #define MAX_LINHAS 100
 #define MAX_COLUNAS 100
-#define DIREITA '>'
-#define ESQUERDA '<'
-#define CIMA '^'
-#define BAIXO 'v'
 #define PAREDE '#'
 #define COMIDA '*'
 #define VAZIO ' '
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+#define CIMA 'w'
+#define BAIXO 's'
+#define FANTASMA_B 'B'
+#define FANTASMA_P 'P'
+#define FANTASMA_I 'I'
+#define FANTASMA_C 'C'
+/*
+typedef struct ranking{
+    char direcao;
+    int pontos;
+    int colisao;
+    int movimentos;
+}tRanking;
 
+tRanking RankingCriar(char direcao, int pontos, int colisao, int movimentos);
+void RankingImprimir(tRanking ranking);
+tRanking RankingAtualizar(tRanking ranking, char direcao, int pontos, int colisao, int movimentos);
+void RankingOrdenar(int tam, tRanking ranking[tam]);
+void RankingGerarArquivo(int tam, tRanking ranking[tam], char *diretorio);
+*/
 
-typedef struct fantasma{
+typedef struct fantasma
+{
     int linha;
     int coluna;
     char fantasma;
     char sentido;
-}tFantasma;
+} tFantasma;
 
-/*
-    Funcao: fantasma_criar
-    Descricao: Cria um fantasma
-    Parametros: linha - linha que o fantasma está
-                coluna - coluna que o fantasma está
-                fantasma - fantasma
-    Retorno: fantasma - fantasma criado
-*/
-tFantasma fantasma_criar(int linha, int coluna, char fantasma);
+tFantasma FantasmaCriar(int linha, int coluna, char fantasma);
+int EhFantasma(tFantasma fantasma, int linha, int coluna);
+int FantasmaLinha(tFantasma fantasma);
+int FantasmaColuna(tFantasma fantasma);
+char FantasmaSentido(tFantasma fantasma);
+tFantasma FantasmaAtualizar(tFantasma fantasma, int linha, int coluna, char sentido);
 
-/*
-    Funcao: fantasma_exibir
-    Descricao: Exibe o fantasma na tela
-    Parametros: fantasma - fantasma
-*/
-void fantasma_exibir(tFantasma fantasma);
-
-/*
-    Funcao: fantasma_atualizar_posicao
-    Descricao: Atualiza a posicao do fantasma no mapa
-    Parametros: fantasma - fantasma
-                linha - linha que o fantasma está
-                coluna - coluna que o fantasma está
-                sentido - sentido que o fantasma está indo
-    Retorno: fantasma - fantasma atualizado
-*/
-tFantasma fantasma_atualizar_posicao(tFantasma fantasma, int linha, int coluna, char sentido);
-
-/*
-    Funcao: fantasma_sentido
-    Descricao: Retorna o sentido que o fantasma está indo
-    Parametros: fantasma - fantasma
-    Retorno: sentido - sentido que o fantasma está indo
-*/
-char fantasma_sentido(tFantasma fantasma);
-
-typedef struct pacMan{
+typedef struct pacMan
+{
     int linha;
     int coluna;
     char pacMan;
-}tPacMan;
+} tPacMan;
 
-/*
-    Funcao: pacMan_criar
-    Descricao: Cria um pacman
-    Parametros: linha - linha que o pacman está
-                coluna - coluna que o pacman está
-                pacMan - pacman
-    Retorno: pacMan - pacman criado
-*/
-tPacMan pacMan_criar(int linha, int coluna, char pacMan);
+tPacMan PacManCriar(int linha, int coluna);
+int EhPacMan(tPacMan pacMan, int linha, int coluna);
+int PacManLinha(tPacMan pacMan);
+int PacManColuna(tPacMan pacMan);
+tPacMan PacManAtualizar(tPacMan pacMan, int linha, int coluna);
 
-/*
-    Funcao: pacMan_atualizar_posicao
-    Descricao: Atualiza a posicao do pacman no mapa
-    Parametros: pacMan - pacman
-                linha - linha que o pacman está
-                coluna - coluna que o pacman está
-                direcao - direcao que o pacman está indo
-    Retorno: pacMan - pacman atualizado
-*/
-tPacMan pacMan_atualizar_posicao(tPacMan pacMan, int linha, int coluna, char direcao);
-
-/*
-    Funcao: pacMan_exibir
-    Descricao: Exibe o pacman na tela
-    Parametros: pacMan - pacman
-*/
-void pacMan_exibir(tPacMan pacMan);
-
-typedef struct mapa{
+typedef struct mapa
+{
     int linhas;
     int colunas;
     char matriz[MAX_LINHAS][MAX_COLUNAS];
-}tMapa;
+} tMapa;
 
-/*
-    Funcao: mapa_carregar
-    Descricao: Carrega o mapa do arquivo mapa.txt se o arquivo existir, senao encerra o programa e exibe uma mensagem de erro 
-    Parametros: diretorio - pasta onde está o mapa.txt - argv[?]
-    Retorno: mapa - mapa carregado
-*/
-tMapa mapa_carregar(char *diretorio);
+tMapa MapaCriar(FILE *arquivo, int linhas, int colunas);
+void MapaImprimir(tMapa mapa, tPacMan pacMan, int tamFantasma, tFantasma fantasmas[tamFantasma]);
+int EhParede(tMapa mapa, int linha, int coluna);
+int EhComida(tMapa mapa, int linha, int coluna);
+int EhPacManMapa(tMapa mapa, int linha, int coluna);
+int EhFantasmaMapa(tMapa mapa, int linha, int coluna);
+tPacMan MapaAchaPacMan(tMapa mapa);
+int MapaAchaFantasmas(tMapa mapa, tFantasma fantasmas[MAX_FANTASMAS]);
+tMapa MapaTiraComida(tMapa mapa, int linha, int coluna);
+int MapaNumeroLinhas(tMapa mapa);
+int MapaNumeroColunas(tMapa mapa);
+int MapaNumeroComidas(tMapa mapa);
 
-/*
-    Funcao: mapa_gerar_arquivo_inicializacao
-    Descricao: Gera o arquivo inicializacao.txt com as informacoes do mapa e onde o pacman comeca
-    Parametros: mapa - mapa carregado
-                diretorio - pasta onde criará o arquivo inicializacao.txt - argv[?]
-*/
-void mapa_gerar_arquivo_inicializacao(tMapa mapa, char *diretorio);
-
-/*
-    Funcao: mapa_exibir
-    Descricao: Exibe o mapa na tela e o pacMan e os fantasmas
-    Parametros: mapa, pacMan, fantasmas e quantidade de fantasmas
-*/
-void mapa_exibir(tMapa mapa, tPacMan pacMan, tFantasma fantasmas[], int quantidadeFantasmas);
-
-/*
-    Funcao: mapa_celula
-    Descricao: Retorna o que tem na celula do mapa
-    Parametros: mapa - mapa carregado
-                linha - linha que o pacman está
-                coluna - coluna que o pacman está
-    Retorno: celula - o que tem na celula do mapa
-*/
-char mapa_celula(tMapa mapa, int linha, int coluna);
-
-typedef struct jogo{
+typedef struct jogo
+{
     tMapa mapa;
     tPacMan pacMan;
     tFantasma fantasmas[MAX_FANTASMAS];
+    int tamFantasmas;
+    int numComidas;
     int limiteMovimentos; // numero maximo de movimentos que o pacman pode fazer
-    int movimentos; // numero de movimentos que o pacman fez
-    int pontos; // numero de pontos que o pacman fez
-    int vivo ; // 1 - vivo, 0 - morto
-}tJogo;
+    int movimentos;       // numero de movimentos que o pacman fez
+    int pontos;           // numero de pontos que o pacman fez
+    int vivo;             // 1 - vivo, 0 - morto
+} tJogo;
 
-/*
-    Funcao: jogo_inicializar
-    Descricao: Inicializa o jogo
-    Parametros: diretorio - pasta onde está o mapa.txt - argv[?]
-    Retorno: jogo - jogo inicializado
-*/
-tJogo jogo_inicializar(char *diretorio);
+tJogo JogoCriar(char *diretorio);
+void JogoImprimir(tJogo jogo, char movimento);
+char JogoLerJogada();
+tJogo JogoMoverPacMan(tJogo jogo, char movimento);
+tJogo JogoMoverFantasmas(tJogo jogo);
+int JogoAcabou(tJogo jogo);
+void JogoGerarResumo(tJogo jogo);
 
-/*
-    Funcao: jogo_ler_jogada
-    Descricao: Le a jogada do usuario
-    Retorno: direcao - direcao que o pacman está indo < ^ > v
-*/
-char jogo_ler_jogada();
-
-/*
-    Funcao: jogo_processa_jogada
-    Descricao: Processa a jogada do usuario, anda com o pacman no mapa e atualiza o jogo
-    Parametros: jogo - jogo inicializado
-                direcao - direcao que o pacman está indo
-    Retorno: jogo - jogo atualizado
-*/
-tJogo jogo_processa_jogada(tJogo jogo, char direcao);
-
-/*
-    Funcao: jogo_move_fantasmas
-    Descricao: Move os fantasmas no mapa
-    Parametros: jogo - jogo inicializado
-    Retorno: jogo - jogo atualizado
-*/
-tJogo jogo_move_fantasmas(tJogo jogo);
-
-/*
-    Funcao: jogo_verificar_fim
-    Descricao: Verifica se o jogo acabou
-    Parametros: jogo - jogo inicializado
-    Retorno: 1 - jogo acabou
-             0 - jogo nao acabou
-*/
-int jogo_verificar_fim(tJogo jogo);
-
-/*
-    Funcao: jogo_exibir
-    Descricao: Exibe o jogo na tela
-    Parametros: jogo - jogo inicializado
-*/
-void jogo_exibir(tJogo jogo);
-
-/*
-    Funcao: jogo_finalizar
-    Descricao: Finaliza o jogo e exive as mensagens de vitoria ou derrota
-    Parametros: jogo - jogo inicializado
-*/
-void jogo_finalizar(tJogo jogo);
-
-int main(int argc, char *argv[]){
-    if(argc < 2){
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         printf("Erro: Informe o diretorio do mapa\n");
         return 1;
     }
 
-    tJogo jogo = jogo_inicializar(argv[1]);
-    while(!jogo_verificar_fim(jogo)){
-        jogo_exibir(jogo);
-        char direcao = jogo_ler_jogada();
-        jogo = jogo_processa_jogada(jogo, direcao);
-        jogo = jogo_move_fantasmas(jogo);
-    }
+    tJogo jogo = JogoCriar(argv[1]);
 
-    jogo_finalizar(jogo);
+    while (!JogoAcabou(jogo))
+    {
+        char movimento = JogoLerJogada();
+        jogo = JogoMoverPacMan(jogo, movimento);
+        jogo = JogoMoverFantasmas(jogo);
+        JogoImprimir(jogo, movimento);
+        // JogoGerarResumo(jogo);
+    }
 
     return 0;
 }
+
+tJogo JogoCriar(char *diretorio)
+{
+    char *arquivoMapa;
+    sprintf(arquivoMapa, "%s/mapa.txt", diretorio);
+    FILE *arq = fopen(arquivoMapa, "r");
+
+    if (arq == NULL)
+    {
+        printf("Erro: Arquivo %s nao encontrado\n", arquivoMapa);
+        exit(1);
+    }
+
+    tJogo jogo;
+    int linhas, colunas, limiteMovimentos;
+    fscanf(arq, "%d %d %d%*c", &linhas, &colunas, &limiteMovimentos);
+
+    jogo.mapa = MapaCriar(arq, linhas, colunas);
+    jogo.pacMan = MapaAchaPacMan(jogo.mapa);
+    jogo.tamFantasmas = MapaAchaFantasmas(jogo.mapa, jogo.fantasmas);
+    jogo.numComidas = MapaNumeroComidas(jogo.mapa);
+    jogo.limiteMovimentos = limiteMovimentos;
+    jogo.movimentos = 0;
+    jogo.pontos = 0;
+    jogo.vivo = 1;
+
+    fclose(arq);
+    return jogo;
+}
+
+void JogoImprimir(tJogo jogo, char movimento)
+{
+    printf("Estado do jogo apos o movimento '%c':\n", movimento);
+    MapaImprimir(jogo.mapa, jogo.pacMan, jogo.tamFantasmas, jogo.fantasmas);
+    printf("Pontuacao: %d\n", jogo.pontos);
+}
+
+char JogoLerJogada()
+{
+    char movimento;
+    scanf("%c%*c", &movimento);
+    return movimento;
+}
+/*
+tJogo JogoMoverPacMan(tJogo jogo, char movimento)
+{
+    int linha = PacManLinha(jogo.pacMan);
+    int coluna = PacManColuna(jogo.pacMan);
+
+    switch (movimento)
+    {
+    case DIREITA:
+        coluna++;
+        break;
+    case ESQUERDA:
+        coluna--;
+        break;
+    case CIMA:
+        linha--;
+        break;
+    case BAIXO:
+        linha++;
+        break;
+    }
+
+    if (EhParede(jogo.mapa, linha, coluna))
+    {
+        jogo.vivo = 0;
+        return jogo;
+    }
+
+    if (EhComida(jogo.mapa, linha, coluna))
+    {
+        jogo.mapa = MapaTiraComida(jogo.mapa, linha, coluna);
+        jogo.pontos++;
+        jogo.numComidas--;
+    }
+
+    if (EhFantasmaMapa(jogo.mapa, linha, coluna))
+    {
+        jogo.vivo = 0;
+    }
+
+    jogo.pacMan = PacManAtualizar(jogo.pacMan, linha, coluna);
+    jogo.movimentos++;
+
+    return jogo;
+}
+
+tJogo JogoMoverFantasmas(tJogo jogo){
+    int i;
+    for(i = 0; i < jogo.tamFantasmas; i++){
+        int linha = FantasmaLinha(jogo.fantasmas[i]);
+        int coluna = FantasmaColuna(jogo.fantasmas[i]);
+        char sentido = FantasmaSentido(jogo.fantasmas[i]);
+
+        switch (sentido)
+        {
+        case DIREITA:
+            coluna++;
+            break;
+        case ESQUERDA:
+            coluna--;
+            break;
+        case CIMA:
+            linha--;
+            break;
+        case BAIXO:
+            linha++;
+            break;
+        }
+
+        if (EhParede(jogo.mapa, linha, coluna))
+        {
+            char sentidoOposto;
+            switch (sentido)
+            {
+            case DIREITA:
+                sentidoOposto = ESQUERDA;
+                break;
+            case ESQUERDA:
+                sentidoOposto = DIREITA;
+                break;
+            case CIMA:
+                sentidoOposto = BAIXO;
+                break;
+            case BAIXO:
+                sentidoOposto = CIMA;
+                break;
+            }
+            jogo.fantasmas[i] = FantasmaAtualizar(jogo.fantasmas[i], linha, coluna, sentidoOposto);
+        }
+
+        if (EhPacMan(jogo.pacMan, linha, coluna))
+        {
+            jogo.vivo = 0;
+        }
+
+        jogo.fantasmas[i] = FantasmaAtualizar(jogo.fantasmas[i], linha, coluna, sentido);
+    }
+    return jogo;
+}
+
+int JogoAcabou(tJogo jogo)
+{
+    if (jogo.vivo == 0)
+    {
+        printf("Game over!\n");
+        return 1;
+    }
+    else if (jogo.numComidas == 0)
+    {
+        printf("Voce venceu!\n");
+        return 1;
+    }
+    else if (jogo.movimentos == jogo.limiteMovimentos)
+    {
+        printf("Game over!\n");
+        return 1;
+    }
+    return 0;
+}
+*/
